4fcbb74df367a19faefbc73cb7ed3e2461f4483d
Karl Krach <github@kkrach.de>
2022-01-30 17:51:33 +0100

    Reduce unsafe block

--- safe_foo/src/lib.rs
+++ safe_foo/src/lib.rs
@@ -5,34 +5,36 @@ pub fn foo_read_file(filename: &str, content: &mut Vec<u8>) -> bool {
     let c_filename = std::ffi::CString::new(filename).expect("CString::new failed");
     let buffer = content.as_mut_ptr() as *mut _;
     let mut buflen: raw_foo::size_t = content.len() as _;
+    let result;
 
     unsafe {
-        let result = raw_foo::foo_read_file(c_filename.as_ptr(), buffer, &mut buflen);
-        match result {
-            raw_foo::FOO_Error_FOO_INVALID_ARGUMENT => {
-                println!("Error: Internal error!");
-                content.resize(0, 0);
-                false
-            }
-            raw_foo::FOO_Error_FOO_FILE_NOT_FOUND => {
-                println!("Error: File '{}' not found!", filename);
-                content.resize(0, 0);
-                false
-            }
-            raw_foo::FOO_Error_FOO_ERROR_READ => {
-                println!("Error: Failed reading file {}!", filename);
-                content.resize(0, 0);
-                false
-            }
-            raw_foo::FOO_Error_FOO_OK => {
-                content.resize(buflen as usize, 0);
-                true
-            }
-            _ => {
-                println!("Error: Unknown error when reading {}!", filename);
-                content.resize(0, 0);
-                false
-            }
+        result = raw_foo::foo_read_file(c_filename.as_ptr(), buffer, &mut buflen);
+    }
+
+    match result {
+        raw_foo::FOO_Error_FOO_INVALID_ARGUMENT => {
+            println!("Error: Internal error!");
+            content.resize(0, 0);
+            false
+        }
+        raw_foo::FOO_Error_FOO_FILE_NOT_FOUND => {
+            println!("Error: File '{}' not found!", filename);
+            content.resize(0, 0);
+            false
+        }
+        raw_foo::FOO_Error_FOO_ERROR_READ => {
+            println!("Error: Failed reading file {}!", filename);
+            content.resize(0, 0);
+            false
+        }
+        raw_foo::FOO_Error_FOO_OK => {
+            content.resize(buflen as usize, 0);
+            true
+        }
+        _ => {
+            println!("Error: Unknown error when reading {}!", filename);
+            content.resize(0, 0);
+            false
         }
     }
 }
